c4eeb47ac28122c1e11ce9276df46e75944f41c2
njaboy <njaboy@live.com>
2018-06-05 15:47:50 -0500

    new scripts

---
+++ boolean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $1 -gt 100 ]
+then
+    echo "'Thats a Large number"
+fi
+    echo Your nuber was $1
+
---
+++ input.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'What are your favorite fruits'
+read fruit1 fruit2 fruit3
+
+echo Your first fruit was $fruit1 $fruit2 $fruit3
+
